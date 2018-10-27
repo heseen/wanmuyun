acb0ea1c8ce49b4f600bb092023d5140091963cb
Jason029 <hiseen@qq.com>
2018-10-27 22:13:58 +0800

    update

---
+++ design/01/index.php
@@ -0,0 +1,20 @@
+<?php
+//index.php
+
+echo("hello world")
+
+if (condition) {
+    # code...
+} else {
+    # code...
+}
+$i=1;
+while ($i <= 100) {
+    echo $i,'<br/>';
+    $i=$i+1;
+}
+
+echo(date("Y-m-d",time()));
+
+
+?>
